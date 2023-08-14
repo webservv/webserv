041cdc81be3149b31718ec6d63201a03ea5f14b0
yoonsele <yoonseo9800@gmail.com>
2023-08-14 20:02:58 +0900

    make client.cpp, add receiveFromSocket method in server, test echo server(video), add client_socketfd in server.hpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,22 +1,23 @@
 #include "includes.hpp"
 
 int main() {
-    const int port = 8080;
-    const char* host = "0.0.0.0";
+	const int port = 8080;
+	const char* host = "127.0.0.1";
 
-    try {
-        Server& server = Server::getInstance(port, host);
-        std::cout << "Server started, waiting for connections..." << std::endl;
+	try {
+		Server& server = Server::getInstance(port, host);
+		std::cout << "Server started, waiting for connections..." << std::endl;
 
-        while (true) {
-            server.acceptConnection();
-            server.handlePoll();
-        }
-        server.stop();
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+		while (true) {
+			server.acceptConnection();
+			server.receiveFromSocket();
+			// server.handlePoll();
+		}
+		server.stop();
+	} catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
 
-    return 0;
+	return 0;
 }
