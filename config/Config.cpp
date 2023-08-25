e772483694ed7d8c11a4fbc2aa4fea91e7618de9
Minju Kim <peppertones0123@gmail.com>
2023-08-25 18:49:25 +0900

    parsing config

--- config/Config.cpp
+++ config/Config.cpp
@@ -3,35 +3,70 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <sstream>
 
 Config::Config(const std::string& config_file) 
 {
 	std::fstream configParser;
 	configParser.open(config_file);
-
-	std::string line;
-	while (std::getline(configParser, line)) {
-		if (line.front() == '#' || line.length() == 0)
-			continue;
-		if (line.back() != ';' && line.back() != '{' && line.back() != '}') {
-			std::cout << line << std::endl;
-			throw std::out_of_range("invalid config - line");
-		}
-		tokenization(line);
-	}
-
+    if (!configParser.is_open()) {
+        throw std::runtime_error("config file open error");
+    }
+    parseLine(configParser);
 	while (!tokens.empty()) {
-		if (tokens.front() == "http") {
-			tokens.pop();
+		std::string token = tokens.front();
+		tokens.pop();
+
+		if (token == "http") {
 			if (tokens.front() == "{") {
 				tokens.pop();
 				parseHTTP();
-			}
-		}
-		else
+			} else {
+                throw std::out_of_range("missing '{' after http");
+            }
+		} else {
 			throw std::out_of_range("invalid config - constructor");
+		}
 	}
 }
+
+void Config::parseLine(std::fstream& configParser) {
+	std::string line;
+	while (std::getline(configParser, line)) {
+		size_t commentPos = line.find('#');
+		if (commentPos != std::string::npos) {
+			line = line.substr(0, commentPos);
+		}
+		line = trim(line);
+		if (line.empty())
+			continue;
+		if (line.back() != ';' && line.back() != '{' && line.back() != '}') {
+			throw std::out_of_range("invalid config - line");
+		}
+		tokenization(line);
+    }
+}
+
+std::string Config::trim(const std::string &str)
+{
+    std::string::const_iterator start = str.begin();
+    while (start != str.end() && std::isspace(*start)) {
+        start++;
+    }
+
+    if (start == str.end()) {
+        return "";
+    }
+
+    std::string::const_iterator end = str.end();
+    end--;
+    while (end != start && std::isspace(*end)) {
+        end--;
+    }
+
+    return std::string(start, end + 1);
+}
+
 
 void Config::tokenization(const std::string& line) {
 
@@ -40,136 +75,284 @@ void Config::tokenization(const std::string& line) {
 	while (std::getline(tokenStream, token, ' ')) {
 		if (!token.empty()) {
 			tokens.push(token);
-			// std::cout << "Token: " << token << std::endl;
 		}
 	}
 }
 
+
 void Config::parseHTTP(void) {
 	while (tokens.front() != "}") {
-		if (tokens.front() == "server") {
-			tokens.pop();
-			if (tokens.front() == "{") {
-				tokens.pop();
-				parseServer();
-			}
-		}
-		else
+        std::string title = tokens.front();
+        tokens.pop();
+		if (title == "server")
+			parseServer();
+		else if (title == "client_max_body_size")
+            parseClientMaxBodySize();
+		else {
 			throw std::out_of_range("invalid config - http");
+		}
 	}
 	tokens.pop();
 }
 
+void Config::parseClientMaxBodySize() {
+    std::string valueStr = tokens.front();
+    tokens.pop();
+
+    if (valueStr.back() != ';') {
+        throw std::out_of_range("missing ';' after client_max_body_size");
+    }
+    valueStr.pop_back();
+
+    std::stringstream ss(valueStr);
+    ss >> clientMaxBodySize;
+    if (ss.fail() || clientMaxBodySize < 0) {
+        throw std::out_of_range("invalid client_max_body_size, must be non-negative");
+    }
+}
+
+
+// listen, server_name, error_page, root, index, location
 void Config::parseServer(void) {
 
 	server new_server;
 	new_server.clinetMaxSize = 1;
 	std::string buf;
 
+    if (tokens.front() == "{") {
+        tokens.pop();
+    } else {
+        throw std::out_of_range("missing '{' after server");
+    }
 	while (tokens.front() != "}") {
-		if (tokens.front() == "listen") {
-			tokens.pop();
-			buf = tokens.front();
-			buf.pop_back();
-			new_server.listen_port = std::stoi(buf);
-			tokens.pop();
-		}
-		else if (tokens.front() == "server_name") {
-			tokens.pop();
-			buf = tokens.front();
-			buf.pop_back();
-			new_server.server_name = buf;
-			tokens.pop();
-		}
-		else if (tokens.front() == "location") {
-			tokens.pop();
-			parseLocation(new_server);
-		}
-		else if (tokens.front() == "error_page") {
-			tokens.pop();
+        std::string title = tokens.front();
+        tokens.pop();
+		if (title == "listen")
+            parseListen(new_server);
+		else if (title == "server_name")
+            parseServerName(new_server);
+		else if (title == "error_page")
 			parseErrorPage(new_server);
-		}
-		else if (tokens.front() == "client_max_body_size") {
-			tokens.pop();
-			std::string size = tokens.front();
-			tokens.pop();
-			size.pop_back();
-			if (size.back() == 'M') {
-				size.pop_back();
-				new_server.clinetMaxSize = std::stoi(size);
-			}
-			else
-				throw std::out_of_range("invalid config - max size");
-		}
-		else
+        else if (title == "root")
+            parseRoot(new_server);
+        else if (title == "index")
+            parseIndex(new_server);
+		else if (title == "location")
+            parseLocation(new_server);
+		else {
+
 			throw std::out_of_range("invalid config - server");
+        }
 	}
 	servers.push_back(new_server);
 	tokens.pop();
 }
 
+void Config::parseListen(server &new_server) {
+    std::string port = tokens.front();
+    tokens.pop();
+    if (port.back() != ';') {
+        throw std::out_of_range("missing ';' after port number");
+    }
+    port.pop_back();
+    std::stringstream ss(port);
+    ss >> new_server.listen_port;
+    if (ss.fail()) {
+        throw std::out_of_range("invalid port number");
+    }
+}
+
+void Config::parseServerName(server &new_server) {
+    std::string name = tokens.front();
+    tokens.pop();
+    if (name.back() != ';') {
+        throw std::out_of_range("missing ';' after server name");
+    }
+    name.pop_back();
+    new_server.server_name = name;
+}
+
+
+void Config::parseErrorPage(server& new_server) {
+    std::string errorCodeStr = tokens.front();
+    tokens.pop();
+
+    int errorCode;
+    std::stringstream ss1(errorCodeStr);
+    ss1 >> errorCode;
+    if (ss1.fail()) {
+        throw std::out_of_range("invalid error code");
+    }
+
+    std::string errorPage = tokens.front();
+    tokens.pop();
+    if (errorPage.back() != ';') {
+        throw std::out_of_range("missing ';' after error page");
+    }
+    errorPage.pop_back();
+
+    new_server.errorPages.insert(std::make_pair(errorCode, errorPage));
+}
+
+void Config::parseRoot(server &new_server) {
+    std::string path = tokens.front();
+    if (path.back() != ';') {
+        throw std::out_of_range("missing ';' after root path");
+    }
+    path.pop_back();
+    new_server.root_path = path;
+    tokens.pop();
+}
+
+void Config::parseIndex(server &new_server) {
+    std::string indexFile = tokens.front();
+    if (indexFile.back() != ';') {
+        throw std::out_of_range("missing ';' after index file");
+    }
+    indexFile.pop_back();
+    new_server.index_file = indexFile;
+    tokens.pop();
+}
+
+// limit_except try_files root autoindex include fastcgi_pass fastcgi_param
 void Config::parseLocation(server& server) {
 	
 	location new_location;
 	
-	new_location.url = tokens.front();
-	tokens.pop();
+    if (tokens.front() == "~") {
+        tokens.pop();
+        new_location.is_regex = true;
+    } else {
+        new_location.is_regex = false;
+    }
 
-	if (tokens.front() == "{") {
-		tokens.pop();
-	}
+    new_location.url = tokens.front();
+    tokens.pop();
+
+    if (tokens.front() != "{") {
+        throw std::out_of_range("missing '{' after location");
+    }
+    tokens.pop();
 	std::string buf;
-
 	while (tokens.front() != "}") {
 		std::string key = tokens.front();
-		if (key == "root" || key == "index" || key == "return" || key == "upload_store") {
-			tokens.pop();
-			std::string value = tokens.front();
-			value.pop_back();
-			new_location.info[key] = value;
-			tokens.pop();
-		}
-		else if (tokens.front() == "limit_except") {
-			tokens.pop();
-			while (tokens.front().back() != ';') {
-				new_location.allowedMethod.push_back(tokens.front());
-				tokens.pop();
-			}
-			std::string method = tokens.front();
-			method.pop_back();
-			new_location.allowedMethod.push_back(method);
-			tokens.pop();
-		}
-		else if (tokens.front() == "autoindex") {
-			tokens.pop();
-			if (tokens.front() == "on;")
-				new_location.autoIndex = true;
-			else if (tokens.front() == "off;")
-				new_location.autoIndex = false;
-			else
-				throw std::out_of_range("invalid config - autoindex");
-			tokens.pop();
-		}
-		else
-			throw std::out_of_range("invalid config - location");
+        tokens.pop();
+        if (key == "limit_except") {
+            parseLimitExcept(new_location);
+        } else if (key == "try_files") {
+            parseTryFiles(new_location);
+        } else if (key == "root") {
+            parseRoot(new_location);
+        } else if (key == "autoindex") {
+            parseAutoIndex(new_location);
+        } else if (key == "include") {
+            parseInclude(new_location);
+        } else if (key == "fastcgi_pass") {
+            parseFastcgiPass(new_location);
+        } else if (key == "fastcgi_param") {
+            parseFastcgiParam(new_location);
+        } else if (key == "return") {
+            parseReturn(new_location);
+        } else {
+            std::cout << key << std::endl;
+            throw std::out_of_range("invalid config - location");
+        }
 	}
 	tokens.pop();
 	server.locations.push_back(new_location);
 }
 
-void Config::parseErrorPage(server& server) {
-	
-	int errorCode = std::stoi(tokens.front());
-	tokens.pop();
-	std::string errorPage = tokens.front();
-	errorPage.pop_back();
-	tokens.pop();
-	server.errorPages.insert(std::pair<int, std::string>(errorCode, errorPage));
+void Config::parseLimitExcept(location& loc) {
+    while (tokens.front().back() != ';') {
+        loc.allowedMethod.push_back(tokens.front());
+        tokens.pop();
+    }
+    std::string lastMethod = tokens.front();
+    if (lastMethod.back() != ';') {
+        throw std::out_of_range("missing ';' after last method");
+    }
+    lastMethod.pop_back();
+    loc.allowedMethod.push_back(lastMethod);
+    tokens.pop();
+}
+
+
+void Config::parseTryFiles(location& loc) {
+    while (tokens.front().back() != ';') {
+        loc.try_files.push_back(tokens.front());
+        tokens.pop();
+    }
+    std::string lastFile = tokens.front();
+    lastFile.pop_back(); // Removing trailing ';'
+    loc.try_files.push_back(lastFile);
+    tokens.pop();
+}
+
+void Config::parseRoot(location& loc) {
+    std::string path = tokens.front();
+    if (path.back() != ';') {
+        throw std::out_of_range("missing ';' after root path");
+    }
+    path.pop_back();
+    loc.root = path;
+    tokens.pop();
+}
+
+void Config::parseAutoIndex(location& loc) {
+    std::string value = tokens.front();
+    value.pop_back();
+    loc.autoindex = (value == "on");
+    tokens.pop();
+}
+
+void Config::parseInclude(location& loc) {
+    std::string file = tokens.front();
+    file.pop_back();
+    loc.include_file = file;
+    tokens.pop();
+}
+
+void Config::parseFastcgiPass(location& loc) {
+    std::string path = tokens.front();
+    path.pop_back();
+    loc.fastcgi_pass = path;
+    tokens.pop();
+}
+
+void Config::parseFastcgiParam(location& loc) {
+    std::string key = tokens.front();
+    tokens.pop();
+    std::string value = tokens.front();
+    value.pop_back();
+    loc.fastcgi_params[key] = value;
+    tokens.pop();
+}
+
+void Config::parseReturn(location& loc) {
+    std::stringstream ss(tokens.front());
+    ss >> loc.return_code;
+    if (ss.fail() || loc.return_code < 100 || loc.return_code > 599) {
+        throw std::out_of_range("invalid return code");
+    }
+    tokens.pop();
+
+    loc.return_url = tokens.front();
+    if (loc.return_url.back() != ';') {
+        throw std::out_of_range("missing ';' after return URL");
+    }
+    loc.return_url.pop_back();
+    tokens.pop();
+
+    if (loc.return_url.empty()) {
+        throw std::out_of_range("return URL must not be empty");
+    }
 }
 
 Config::~Config() {}
 Config::Config(const Config& copy) {
+    static_cast<void>(copy);
 }
+
 Config& Config::operator=(const Config& copy) {
 	static_cast<void>(copy);
 	return *this;
--- config/Config.hpp
+++ config/Config.hpp
@@ -9,17 +9,27 @@
 #include <map>
 
 struct location {
-	std::map<std::string, std::string> info;
-	std::string url;
-	std::vector<std::string> allowedMethod;
-	bool autoIndex;
+    std::string url;
+    bool is_regex;
+    std::vector<std::string> allowedMethod;
+    std::vector<std::string> try_files;
+    std::string root;
+    bool autoindex;
+    std::string include_file;
+    std::string fastcgi_pass;
+    std::map<std::string, std::string> fastcgi_params;
+    int return_code;
+    std::string return_url;
 };
+
 
 struct server{
 	int listen_port;
 	std::string server_name;
-	std::vector<location> locations;
 	std::map<int, std::string> errorPages;
+    std::string root_path;
+    std::string index_file;
+	std::vector<location> locations;
 	int clinetMaxSize;
 };
 
@@ -28,20 +38,37 @@ class Config {
 private:
 	std::queue<std::string>		tokens;
 	std::vector<server>			servers;
+    int                         clientMaxBodySize;
 private:
 	Config(const Config& copy);
 	Config&	operator=(const Config& copy);
 
 private:
+    void parseLine(std::fstream& configParser);
 	void parseHTTP();
 	void parseServer();
+    void parseListen(server& server);
+    void parseServerName(server& server);
+    void parseErrorPage();
+    void parseRoot(server& server);
+    void parseIndex(server& server);
+    void parseClientMaxBodySize();
 	void parseLocation(server& server);
 	void parseErrorPage(server& server);
 	void tokenization(const std::string& line);
+    void parseLimitExcept(location& location);
+    void parseTryFiles(location& location);
+    void parseRoot(location& location);
+    void parseAutoIndex(location& location);
+    void parseInclude(location& location);
+    void parseFastcgiPass(location& location);
+    void parseFastcgiParam(location& location);
+    void parseReturn(location& location);
+    std::string trim(const std::string &str);
 
 public:
-	Config (const std::string& config_file);
-	~Config ();
+	Config(const std::string& config_file);
+	~Config();
 
 };
 
