78c98e8d9a35bc0da82fccc16408a688bad286b7
seonlim <dltjwl1@naver.com>
2023-08-29 15:36:32 +0900

    parse and sync Config

--- config/Config.hpp
+++ config/Config.hpp
@@ -11,63 +11,64 @@
 class Config {
 public:
     struct location {
-    std::string url;
-    bool is_regex;
-    std::vector<std::string> allowedMethod;
-    std::vector<std::string> try_files;
-    std::string root;
-    bool autoindex;
-    std::string include_file;
-    std::string fastcgi_pass;
-    std::map<std::string, std::string> fastcgi_params;
-    int return_code;
-    std::string return_url;
+    std::map<std::string, std::string>  fastcgi_params;
+    std::vector<std::string>            allowedMethod;
+    std::vector<std::string>            try_files;
+    std::string                         url;
+    std::string                         root;
+    std::string                         include_file;
+    std::string                         fastcgi_pass;
+    std::string                         return_url;
+    bool                                is_regex;
+    bool                                autoindex;
+    int                                 return_code;
     };
 struct server{
-	int listen_port;
-	std::string server_name;
-	std::map<int, std::string> errorPages;
-    std::string root_path;
-    std::vector<std::string> index;
-	std::vector<Config::location> locations;
+	std::vector<Config::location>   locations;
+	std::map<int, std::string>      errorPages;
+    std::vector<std::string>        index;
+	std::string                     server_name;
+    std::string                     root_path;
+	int                             listen_port;
     };
 private:
 	std::queue<std::string>		tokens;
 	std::vector<Config::server> servers;
     int                         clientMaxBodySize;
-    bool                        hasHttp;
+    bool                        hasHTTP;
+//Config.cpp
 private:
+    Config();
 	Config(const Config& copy);
-	Config&	operator=(const Config& copy);
-
-private:
-    void parseLine(std::fstream& configParser);
-	void parseHTTP();
-	void parseServer();
-    void parseListen(server& server);
-    void parseServerName(server& server);
-    void parseErrorPage();
-    void parseRoot(server& server);
-    void parseIndex(server& server);
-    void parseClientMaxBodySize();
-	void parseLocation(server& server);
-	void parseErrorPage(server& server);
-	void tokenization(const std::string& line);
-    void parseLimitExcept(location& location);
-    void parseTryFiles(location& location);
-    void parseRoot(location& location);
-    void parseAutoIndex(location& location);
-    void parseInclude(location& location);
-    void parseFastcgiPass(location& location);
-    void parseFastcgiParam(location& location);
-    void parseReturn(location& location);
-    void trim(std::string &str) const;
-
+	Config& operator=(const Config& copy);
 public:
 	Config(const std::string& config_file);
-    const std::vector<server>& getServers() const;
-    int getClientMaxBodySize() const;
 	~Config();
+private:
+    void                        parseLine(std::fstream& configParser);
+	void                        parseHTTP();
+	void                        parseServer();
+    void                        parseListen(server& server);
+    void                        parseServerName(server& server);
+    void                        parseErrorPage();
+    void                        parseRoot(server& server);
+    void                        parseIndex(server& server);
+    void                        parseClientMaxBodySize();
+	void                        parseLocation(server& server);
+	void                        parseErrorPage(server& server);
+    void                        parseLimitExcept(location& location);
+    void                        parseTryFiles(location& location);
+    void                        parseRoot(location& location);
+    void                        parseAutoIndex(location& location);
+    void                        parseInclude(location& location);
+    void                        parseFastcgiPass(location& location);
+    void                        parseFastcgiParam(location& location);
+    void                        parseReturn(location& location);
+    void                        trim(std::string &str) const;
+	void                        tokenization(const std::string& line);
+public:
+    const std::vector<server>&  getServers() const;
+    int                         getClientMaxBodySize() const;
 
 };
 
